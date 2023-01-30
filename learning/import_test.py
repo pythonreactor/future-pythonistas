0f5a08ac386e15087b5fe09718f4d69a06cd5f11
pythonreactor <MJ.Gilbert@icloud.com>
2023-01-29 21:35:12 -0800

    initial push

---
+++ learning/import_test.py
@@ -0,0 +1,22 @@
+# This file is to demonstrate how to import modules and use them in your code.
+
+import io
+import sys
+import math
+
+
+test = math.sqrt(25)
+test1 = math.ceil(25.5)
+test2 = math.floor(25.5)
+
+print(f"The square root of 25 is: {test}")
+print(f"The ceiling of 25.5 is: {test1}")
+print(f"The floor of 25.5 is: {test2}")
+
+
+def test(x):
+    pass
+
+
+def this(t):
+    pass
