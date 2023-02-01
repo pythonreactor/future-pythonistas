590151e2516025e31d59de7c21ecd8b65220253d
pythonreactor <MJ.Gilbert@icloud.com>
2023-01-31 16:08:58 -0800

    Separate student directories and first assignemnt

---
+++ Megan/homework_1/my_first_function.py
@@ -0,0 +1,28 @@
+"""
+For this assignment, I want you to write a function that takes in a list of
+numbers and returns the sum of the numbers in the list.
+
+An example of this in pseudocode would be:
+
+def some_function(list_of_numbers):
+    # do something with the list of numbers
+
+    return sum_of_numbers
+
+
+I would also like for you to create another function under that one that uses
+the first function to return the average of the numbers in the list.
+
+An example of this in pseudocode would be:
+
+def some_other_function(list_of_numbers):
+    # Get the sum of all numbers in the list
+    # Get the average of the numbers in the list (sum / number of numbers)
+
+    return average_of_numbers
+
+"""
+
+# Place your code here:
+
+
