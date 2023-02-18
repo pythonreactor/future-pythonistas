8ff2cd1fbbf281e23d0f9bf789433848ac77d5be
brianelliott <info@elliott.world>
2023-02-17 22:07:19 -0800

    Revert "Have a look"
    This reverts commit f751b11f7f7a940ad47a138878d974859ed7b09f.

--- Brian/homework_1/my_first_function.py
+++ Brian/homework_1/my_first_function.py
@@ -3,7 +3,7 @@ For this assignment, I want you to write a function that takes in a list of
 numbers and returns the sum of the numbers in the list.
 
 An example of this in pseudocode would be:
-z
+
 def some_function(list_of_numbers):
     # do something with the list of numbers
     return sum_of_numbers
