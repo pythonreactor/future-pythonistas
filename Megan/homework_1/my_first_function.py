a53152e600d087c732122986983efc8ad6acbdb7
Megan <meganlucero@gmail.com>
2023-02-19 19:14:41 -0800

    initial commit

--- Megan/homework_1/my_first_function.py
+++ Megan/homework_1/my_first_function.py
@@ -24,6 +24,22 @@ def some_other_function(list_of_numbers):
 """
 
 # Place your code here:
+def sum_numbers(list_of_numbers):
+    return sum(list_of_numbers)
 
+list_of_numbers = (1, 2, 3, 4)
+result = sum_numbers(list_of_numbers)
+print("the sum of the numbers is =", result)
+
+
+from statistics import mean
+
+
+def average(list_of_numbers):
+    return mean(list_of_numbers)
+
+list_of_numbers = (1, 2, 3, 4)
+result = average(list_of_numbers)
+print("the average of the numbers is =", result)
 
 
